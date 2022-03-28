6571c5b0ab8dccc7e038ed9636afaca6bfc4a966
Smith Júnior <smith.junior@icloud.com>
2022-03-28 17:27:45 -0300

    Delete module

--- web/ex_mon/lib/ex_mon_web/controllers/trainers_controller.ex
+++ web/ex_mon/lib/ex_mon_web/controllers/trainers_controller.ex
@@ -1,11 +1,18 @@
 defmodule ExMonWeb.TrainersController do
   use ExMonWeb, :controller
+
   action_fallback ExMonWeb.FallbackController
 
   def create(conn, params) do
     params
     |> ExMon.create_trainer
     |> handle_response(conn)
+  end
+
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ExMon.delete
+    |> handle_delete(conn)
   end
 
   defp handle_response({:ok, trainer}, conn) do
@@ -15,4 +22,12 @@ defmodule ExMonWeb.TrainersController do
   end
 
   defp handle_response({:error, _changeset} = error, _conn), do: error
+
+  defp handle_delete({:ok , _trainer}, conn) do
+    conn
+    |> put_status(:no_content)
+    |> text("")
+  end
+
+  defp handle_delete({:error , _changeset} = error, conn), do: error
 end

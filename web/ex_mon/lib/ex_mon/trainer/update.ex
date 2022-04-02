8332bb8b49daf2e7bc95aade00ae9a33faa46c88
Smith Júnior <smith.junior@icloud.com>
2022-04-01 23:31:20 -0300

    Added the update trainer module

---
+++ web/ex_mon/lib/ex_mon/trainer/update.ex
@@ -0,0 +1,24 @@
+defmodule ExMon.Trainer.Update do
+  alias ExMon.{Trainer, Repo}
+  alias Ecto.UUID
+
+  def call(%{"id" => id} = params) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invalid ID format!"}
+      {:ok, _uuid} -> update(params)
+    end
+  end
+
+  defp update(%{"id" => id} = params) do
+    case Repo.get(Trainer, uuid) do
+      nil -> {:error, "trainer not found!"}
+      trainer -> {:ok, update_trainer(params)}
+    end
+  end
+
+  defp update_trainer(trainer, params) do
+    trainer
+    |> Trainer.changeset(params)
+    |> Repo.update
+  end
+end

be6d3200d42e57f0a0e8d819634e7836f564d125
Smith Júnior <smith.junior@icloud.com>
2022-06-24 23:26:14 -0300

    Fixed unit tests

--- web/ex_mon/lib/ex_mon_web/auth/error_handler.ex
+++ web/ex_mon/lib/ex_mon_web/auth/error_handler.ex
@@ -3,7 +3,7 @@ defmodule ExMonWeb.Auth.ErrorHandler do
 
   @behaviour Guardian.Plug.ErrorHandler
 
-  def auth_error(conn, %{type, _reason}, _opts) do
+  def auth_error(conn, {type, _reason}, _opts) do
     body = Jason.encode!(%{message: to_string(type)})
     send_resp(conn, 401, body)
   end
